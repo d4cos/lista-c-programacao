977a7be47c3905f454afd4fa7d5390e6efac5399
Davi Coutrim <coutrimdavi9@gmail.com>
2026-06-12 13:47:49 -0300

    Create DCC-40.c

---
+++ DCC-40.c
@@ -0,0 +1,18 @@
+// Davi da Costa Coutrim
+// RA: 0028472
+
+#include <stdio.h>
+
+int main() {
+    int num, i = 1;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    while (i <= 10) {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+    }
+
+    return 0;
+}
