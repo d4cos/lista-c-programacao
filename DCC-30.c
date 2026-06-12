7117214945904e419e21dcf366b2df0b0cdc2ab3
Davi Coutrim <coutrimdavi9@gmail.com>
2026-06-12 13:38:40 -0300

    Create DCC-30.c

---
+++ DCC-30.c
@@ -0,0 +1,20 @@
+// Davi da Costa Coutrim
+// RA: 0028472
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    long long fatorial = 1;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    for(i = 1; i <= num; i++) {
+        fatorial *= i;
+    }
+
+    printf("Fatorial = %lld\n", fatorial);
+
+    return 0;
+}
