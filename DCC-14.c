d3a12187323807ecd1d38f4f799a991b450c2ecc
Davi Coutrim <coutrimdavi9@gmail.com>
2026-06-12 13:07:07 -0300

    Update DCC-14.c

--- DCC-14.c
+++ DCC-14.c
@@ -1,1 +1,22 @@
+// Davi da Costa Coutrim
+// RA: 0028472
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+
+    printf("Digite os tres lados do triangulo: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == b && b == c)
+        printf("Triangulo Equilatero\n");
+    else if (a == b || a == c || b == c)
+        printf("Triangulo Isosceles\n");
+    else
+        printf("Triangulo Escaleno\n");
+
+    return 0;
+}
