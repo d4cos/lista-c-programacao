647dd501b8085d3319f41f5a0b7da7b50725eb06
Davi Coutrim <coutrimdavi9@gmail.com>
2026-06-12 13:45:15 -0300

    Create DCC-35.c

---
+++ DCC-35.c
@@ -0,0 +1,21 @@
+// Davi da Costa Coutrim
+// RA: 0028472
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 0, b = 1, c;
+
+    printf("Digite a quantidade de termos: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
