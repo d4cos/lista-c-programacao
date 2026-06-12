42835031b75f56b60292baef6d724ad5a8f150cf
Davi Coutrim <coutrimdavi9@gmail.com>
2026-06-12 13:43:56 -0300

    Create DCC-33.c

---
+++ DCC-33.c
@@ -0,0 +1,15 @@
+// Davi da Costa Coutrim
+// RA: 0028472
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for(i = 1; i <= 30; i++) {
+        if(i % 3 == 0)
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
